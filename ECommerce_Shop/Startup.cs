a31ce469b7706a1d988f89e289a634264c1b6ed9
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-03 10:52:56 +0530

    added the startup.cs file to override owins security

---
+++ ECommerce_Shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ECommerce_Shop.Startup))]
+namespace ECommerce_Shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
